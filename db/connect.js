a8812bae8bf6f1e3ab38b3fdb8dccca0035d7c79
Cameron Woodward <woo23017@byui.edu>
2026-01-20 12:59:06 -0700

    Made fixes

--- db/connect.js
+++ db/connect.js
@@ -1,27 +1,38 @@
 const dotenv = require('dotenv');
-dotenv.config();
-const MongoClient = require('mongodb').MongoClient;
+dotenv.config({override: true});
+const { MongoClient } = require('mongodb');
 
 let _db;
+
+
 
 const initDb = (callback) => {
   if (_db) {
     console.log('Db is already initialized!');
     return callback(null, _db);
   }
-  MongoClient.connect(process.env.MONGODB_URI)
+
+  const uri = process.env.MONGODB_URI;
+
+  if (!uri) {
+    return callback(new Error('MongoDB connection string is not defined in environment variables.'))
+  }
+
+MongoClient.connect(uri)
     .then((client) => {
       _db = client.db();
+      console.log('Connected to MongoDB successfully!');
       callback(null, _db);
     })
     .catch((err) => {
+      console.error('Failed to connect to MongoDB:', err);
       callback(err);
     });
 };
 
 const getDb = () => {
   if (!_db) {
-    throw Error('Db not initialized');
+    throw Error('Db not initialized. Call initDB first');
   }
   return _db;
 };
